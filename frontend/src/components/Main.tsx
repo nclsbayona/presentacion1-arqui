1b5a1c85506d784aa8051bd30e6436a32fbd6c98
dnchaparro <dnchaparro@ms-dnchaparro>
2023-03-29 17:49:11 -0500

    Se hacen ajustes en el compose y en la interfaz
    Se hacen ajustes en el compose debido a que el nombre de la carpeta
    frontend cambio y se agregan la interfaz gráfica

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import UserRestClient from "./UserRestClient";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./Main.css";
+
+export default function Main(props: any){
+
+    let myRestClient = new UserRestClient();
+
+    const [state, setState] = useState({
+        name: "",
+        age: "",
+        birthDay: "",
+        gender: ""
+    });
+
+    const executeSubmit = (event: any) => {
+        event.preventDefault();
+        console.log(state.name);
+        console.log(state.age);
+        console.log(state.birthDay);
+        console.log(state.gender);
+
+        const request = {
+            name: state.name,
+            lastName: state.age,
+            birthDay: state.birthDay,
+            typeID: state.gender,
+        };
+    }
+
+    const handleOnChange = (event: any) => {
+        let nameField = event.target.name;
+        let valueField = event.target.value;
+
+        setState({
+            ...state,
+            [nameField]: valueField
+        });
+    }
+
+    return ( 
+        <>
+        <div className="general">
+            <section className="section">
+                <div className="my-form">
+                    <h1 className="title">Formulario</h1>
+                    <form onSubmit={executeSubmit} id="myform" className="myForm contact-form needs-validation">
+                        <div className="form-group row col-lg-12 col-sm-12 rows">
+                            <label className="col-lg-6" htmlFor="inputName">Nombre</label>
+                            <input className="col-lg-4" type="text" id="inputName" placeholder="Nombre" name="name"
+                                value={state.name} onChange={handleOnChange} required />
+                            <div className="invalid-feedback">
+                                Ingrese un nombre.
+                            </div>        
+                        </div>
+                        <div className="form-group row col-lg-12 col-sm-12 rows">
+                            <label className="col-lg-6" htmlFor="inputName">Apellido</label>
+                            <input className="col-lg-4" type="text" id="inputLastName" placeholder="Apellido" name="lastName"
+                                value={state.name} onChange={handleOnChange} required />
+                            <div className="invalid-feedback">
+                                Ingrese un apellido.
+                            </div>
+                        </div>
+                        <div className="form-group row col-lg-12 col-sm-12 rows">
+                            <label className="col-lg-6" htmlFor="inputName">Edad</label>
+                            <input className="col-lg-4" type="text" id="inputAge" placeholder="Edad" name="age"
+                                value={state.name} onChange={handleOnChange} required />
+                            <div className="invalid-feedback">
+                                Ingrese una edad.
+                            </div>
+                        </div>
+                        <div className="form-group row col-lg-12 col-sm-12 rows">
+                            <label className="col-lg-6" htmlFor="inputName">Genero</label>
+                            <input className="col-lg-4" type="text" id="inputGender" placeholder="Genero" name="gender"
+                                value={state.name} onChange={handleOnChange} required />
+                            <div className="invalid-feedback">
+                                Ingrese un genero.
+                            </div>
+                        </div>
+                        <div className="form-group row col-lg-12 col-sm-12 rows">
+                            <label className="col-lg-6" htmlFor="inputName">Altura</label>
+                            <input className="col-lg-4" type="text" id="inputHeight" placeholder="Altura" name="height"
+                                value={state.name} onChange={handleOnChange} required />
+                            <div className="invalid-feedback">
+                                Ingrese una altura.
+                            </div>
+                        </div>
+                        <button type="submit" className="main-btn">Submit</button>
+                    </form>
+                </div>
+            </section>
+    </div>
+    </>
+    );
+};
